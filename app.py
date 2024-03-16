8dacc13a0b6890b1d5d1eeb35561242019148b75
Guilherme <bittenkourt.gmf@gmail.com>
2024-03-16 17:48:32 -0300

    Pages folder created

--- app.py
+++ app.py
@@ -1,13 +1,1 @@
-import streamlit as st
-from streamlit_gsheets import GSheetsConnection
-import pandas as pd
-
-st.title("Crypto Currency")
-st.markdown("Hello!")
-
-conn = st.connection("gsheets", type=GSheetsConnection)
-
-data = conn.read(worksheet="Data", usecols=list(range(1)), ttl=5)
-data = data.dropna(how="all")
-
-st.dataframe(data)
+from library import *
---
+++ library.py
@@ -0,0 +1,3 @@
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+import pandas as pd
