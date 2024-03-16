8a4bc954090d1113d9703a6f2cd2818da20c9d2c
Guilherme <bittenkourt.gmf@gmail.com>
2024-03-16 17:58:40 -0300

    Dabase Class added

---
+++ pages/📁Registers.py
@@ -0,0 +1,11 @@
+from library import *
+
+st.title("Database")
+st.markdown("Hello!")
+
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+data = conn.read(worksheet="Data", usecols=list(range(7)), ttl=5)
+data = data.dropna(how="all")
+
+st.dataframe(data)
--- pages/🖋️Form.py
+++ pages/🖋️Form.py
@@ -1,11 +1,14 @@
 from library import *
+from services.Database import Database
+
 
 st.title("Crypto Currency")
 st.markdown("Hello!")
 
-conn = st.connection("gsheets", type=GSheetsConnection)
+database = Database(worksheets=[
+    ("Data", 7)
+])
 
-data = conn.read(worksheet="Data", usecols=list(range(7)), ttl=5)
-data = data.dropna(how="all")
+data = database.worksheets["Data"].dropna(how="all")
 
 st.dataframe(data)
