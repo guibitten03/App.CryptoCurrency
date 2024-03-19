5ea391bbe512b5b71e046fb3f3a69e3997b13cb5
Guilherme <bittenkourt.gmf@gmail.com>
2024-03-19 15:43:45 -0300

    Update forms

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from library import *
 from utils.constants import *
+from services.Database import Database
+
+database = Database(worksheets=[
+    ("Data", 7)
+])
+
+sheet = database.worksheets["Data"].dropna(how="all")
 
 st.title("Analysis")
 
 coin = st.multiselect("Select Coin", options=COINS, default="Bitcoin")
+
+st.dataframe(sheet.loc[sheet['Coin'] in coin])
+
+# line_chart = st.line_chart(sheet.loc[sheet['Coin'] in coin])
 
