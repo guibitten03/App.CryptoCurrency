04c2ebfb17efe4604bd894fb48656a5b9e847cce
Guilherme <bittenkourt.gmf@gmail.com>
2024-03-19 16:34:50 -0300

    Update forms

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from library import *
 from utils.constants import *
 from services.Database import Database
 
+st.set_page_config(layout="wide")
+
 database = Database(worksheets=[
     ("Data", 7)
 ])
@@ -10,9 +12,20 @@ sheet = database.worksheets["Data"].dropna(how="all")
 
 st.title("Analysis")
 
-coins = st.multiselect("Select Coin", options=COINS, default="Bitcoin")
+coin = st.selectbox("Select Coin", options=COINS, index=False)
 
-st.dataframe(sheet.loc[sheet[['Coin']] in coins.values])
+line_filtered_df = sheet[(sheet['Coin'] == coin) & (sheet['Status'] == "Buy")]
+mean_price = line_filtered_df['Income'].sum() / line_filtered_df['Amount'].sum()
 
-# line_chart = st.line_chart(sheet.loc[sheet['Coin'] in coin])
+line_filtered_df['Mean Price'] = [mean_price for x in range(line_filtered_df.shape[0])]
 
+c1, c2 = st.columns(2, gap="small")
+
+with c1:
+    line_chart = st.line_chart(line_filtered_df, x="Data", y=["Price", "Mean Price"])
+
+bar_filtered_df = sheet[(sheet['Coin'] == coin)]
+bar_filtered_df = bar_filtered_df.groupby(by="Status")['Income'].sum()
+
+with c2:
+    bar_chart = st.bar_chart(bar_filtered_df)
