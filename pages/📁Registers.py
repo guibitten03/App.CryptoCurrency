644489076fec054a89392341691c28319c9c498d
Guilherme <bittenkourt.gmf@gmail.com>
2024-03-20 16:57:07 -0300

    Teste

--- pages/📁Registers.py
+++ pages/📁Registers.py
@@ -8,8 +8,10 @@ st.markdown("You can see all your trades bellow!")
 
 database = Database(worksheets=[
     ("Data", 7)
+
 ])
 
+# Teste
 data = database.worksheets["Data"].dropna(how="all")
 
 edit_data = st.data_editor(data,hide_index=True)
